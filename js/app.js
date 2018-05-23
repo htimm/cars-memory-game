a868bd98b424eae9798f769bef824407f45e9ad1
Henning Timm <hgtm@me.com>
2018-05-23 23:34:45 +0200

    Adjust restart button color to card background

--- js/app.js
+++
@@ -1,7 +0,0 @@
-
-// function for distributing cards randomly and starting/resetting game
-
-
-document.getElementById("mc1").addEventListener("click", function( event ) {
-    alert('Ich bin Speed!');
-  }, false);
