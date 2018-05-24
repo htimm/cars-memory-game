96db10d14b1ea633674f7f2311da3ad7cc7f54ec
Henning Timm <hgtm@me.com>
2018-05-24 09:46:11 +0200

    Create event listener for restart button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-// Create event listeners for closed cards
+// Create event listener for restart button
+
+const startButton = document.querySelector('#restart');
+startButton.addEventListener('click', function() {
+  alert('You have clicked on the restart button.')
+})
+
+// Create event listeners for closed cards to flip
 
 const closedCards = document.querySelectorAll('.cardback');
 closedCards.forEach(function(element) {
